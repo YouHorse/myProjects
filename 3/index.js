42db1f0deb54308f889500d8ebc28dd34409399d
Senat Balota <senattbalota@gmail.com>
2024-02-11 23:56:19 +0300

    landing page created for the third project

---
+++ 3/index.js
@@ -0,0 +1,10 @@
+let arr = 
+["Yazın gölge hoş, kışın ambar boş",
+"Davulun Sesi Uzaktan Hoş Gelir",
+"Boş yapma",
+"İşin gücün yok mu?"];
+
+function randomPicker() {
+   let answer = document.getElementById("output").innerHTML = arr[(Math.round(Math.random()* arr.length))]
+    
+}
