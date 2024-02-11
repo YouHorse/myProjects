df3e9cb881d7cceb721143e325f7c6b1bf90763b
Senat Balota <senattbalota@gmail.com>
2024-02-11 23:59:25 +0300

    function created to generate random number and used it to calculate random index number

--- 3/index.js
+++ 3/index.js
@@ -3,3 +3,8 @@ let arr =
 "Davulun Sesi Uzaktan Hoş Gelir",
 "Boş yapma",
 "İşin gücün yok mu?"];
+
+function randomPicker() {
+    document.getElementById("output").innerHTML = arr[(Math.round(Math.random()* arr.length))]
+     
+ }
